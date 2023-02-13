ea15e573bac43e6f0be6daccda1f8900033e21e9
Osamuyi <muyixone@yahoo.com>
2023-02-13 15:18:54 +0100

    Setting up the application's basic functionalities

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  // Gets a message from the front end/form
+  socket.on('Chat_message', (message) => {
+    // Sends a message from the server to the front end
+    // socket.broadcast.emit('Chat_message', message);
+    io.emit('Chat_message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(4444, () => {
+  console.log('Server listening on *:4444');
+});
