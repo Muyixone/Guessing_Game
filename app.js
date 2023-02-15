ae5003e97c1ebcc02d27fb514270f4dcc1b96844
Osamuyi <muyixone@yahoo.com>
2023-02-15 10:51:26 +0100

    Move the index.html file to the public directory

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, 'public/html')));
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', (socket) => {
-  // Gets a message from the front end/form
+  // Gets a message from the front-end/form
   socket.on('Chat_message', (message) => {
     // Sends a message from the server to the front end
     // socket.broadcast.emit('Chat_message', message);
