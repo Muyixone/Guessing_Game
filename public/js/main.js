b5a7e5b5f879a0e08f5b2f51dab64dc50c5ce908
Osamuyi <muyixone@yahoo.com>
2023-02-16 03:51:55 +0100

    Add a function for notifying when a user joins

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,18 @@
 var socket = io();
-
+var rando = 'a new user';
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 var messages = document.getElementById('messages');
+//var welcomeMessage = document.getElementById('welcome_message');
+
+//This sends a message to the server about a new user
+socket.emit('User_connected', rando);
+
+socket.on('message', (msg) => {
+  var message = document.createElement('p');
+  message.textContent = msg;
+  messages.appendChild(message);
+});
 
 // Sends a message from the form to the server
 form.addEventListener('submit', (e) => {
@@ -15,7 +25,6 @@ form.addEventListener('submit', (e) => {
 
 // Gets the message from the server and displays it to the from end
 socket.on('Chat_message', (msg) => {
-  console.log(msg);
   var item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
